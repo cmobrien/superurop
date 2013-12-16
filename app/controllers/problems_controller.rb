4ab0c72e79c48df584bb0b1db26215ca7e8d8c6a
Casey O'Brien <cmobrien@mit.edu>
2013-12-15 23:58:30 -0500

    problems

---
+++ app/controllers/problems_controller.rb
@@ -0,0 +1,11 @@
+class ProblemsController < ApplicationController
+
+	def update
+		puts '---------------------------------------'
+		puts '---------------------------------------'
+		puts 'testasdf'
+		puts '---------------------------------------'
+		puts '---------------------------------------'
+		redirect_to '/problems/new'
+	end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Proto2::Application.routes.draw do
+
+  get '/problems/update' => 'problems#update'
+  resources :problems
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
