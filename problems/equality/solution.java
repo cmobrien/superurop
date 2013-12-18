de0154039d40d70d9f3e42f640edb978ed7d7829
Casey O'Brien <cmobrien@mit.edu>
2013-12-18 01:25:11 -0500

    test

--- problems/equality/solution.java
+++ problems/equality/solution.java
@@ -1,6 +1,6 @@
 public class EqualityClass {
   public static boolean intEq(int i1, int i2) {
-    return i1 == i2;
+    return i1 =asd= i2;
   }
 
   public static boolean stringEq(String s1, String s2) {
