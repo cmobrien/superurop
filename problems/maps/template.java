13af485f59e69e1fe3f4cd8a927607cc55edc7ce
Casey O'Brien <cmobrien@mit.edu>
2014-02-05 18:56:51 -0500

    problems

--- problems/maps/template.java
+++ problems/maps/template.java
@@ -3,16 +3,16 @@ import java.util.Map;
 public class MapsClass {
 	public static String get(
     	Map<String, String> M, String v) {
-	// Your Code Here
-  }
+		// Your Code Here
+  	}
     
   public static void append(
   		Map<String, String> M, String v, String k) {
-  	// Your Code Here
-  }
+  		// Your Code Here
+  	}
     
   public static void remove(
     	Map<String, String> M, String v) {
-  	// Your Code Here
-  }
+  		// Your Code Here
+  	}
 }
